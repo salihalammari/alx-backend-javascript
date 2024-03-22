a3b1b38eb79eb2de2281538598967b4257119f48
salihalammari <slammari@student.1337.ma>
2024-03-22 02:23:29 +0000

    done

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
+  const array2 = array;
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    array2[idx] = appendString + value;
   }
 
-  return array;
+  return array2;
 }
