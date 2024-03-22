5cd13073fa666fa618722cd801b3f2f9ce3a5a8c
salihalammari <slammari@student.1337.ma>
2024-03-22 02:11:36 +0000

    done

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,10 +1,5 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  if (expansion1989 === undefined) {
-    expansion1989 = 89;
-  }
+const getSumOfHoods = (initialNumber,
+  expansion1989 = 89,
+  expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
 
-  if (expansion2019 === undefined) {
-    expansion2019 = 19;
-  }
-  return initialNumber + expansion1989 + expansion2019;
-}
+export default getSumOfHoods;
