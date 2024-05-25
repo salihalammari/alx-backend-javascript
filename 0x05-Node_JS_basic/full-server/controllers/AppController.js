6e3f5cc2ab6a02452d74a93a603718e41d2d8df0
salihalammari <slammari@student.1337.ma>
2024-05-25 21:11:02 +0000

    add all

--- 0x05-Node_JS_basic/full-server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full-server/controllers/AppController.js
@@ -1,11 +1,5 @@
-/**
- * Miscellaneous route.
- */
-class AppController {
-  static getHomepage(request, response) {
-    response.status(200).send('Hello Holberton School!');
+export class AppController {
+  static getHomepage(req, res) {
+    res.status(200).send('Hello Holberton School!');
   }
 }
-
-export default AppController;
-module.exports = AppController;
--- 0x05-Node_JS_basic/full-server/routes/index.js
+++ 0x05-Node_JS_basic/full-server/routes/index.js
@@ -1,12 +1,26 @@
-import AppController from '../controllers/AppController';
-import StudentsController from '../controllers/StudentsController';
+import fs from 'fs';
 
-
-const mapRoutes = (app) => {
-  app.get('/', AppController.getHomepage);
-  app.get('/students', StudentsController.getAllStudents);
-  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
-};
-
-export default mapRoutes;
-module.exports = mapRoutes;
+// eslint-disable-next-line import/prefer-default-export
+export const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      reject(err);
+    } else {
+      const lines = data.trim().split('\n');
+      const fields = {};
+      lines.forEach((line, index) => {
+        if (index === 0) {
+          // Skip the header line
+          return;
+        }
+        // eslint-disable-next-line no-unused-vars
+        const [firstName, lastName, age, field] = line.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(`${firstName} ${lastName}`);
+      });
+      resolve(fields);
+    }
+  });
+});
